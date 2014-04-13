c19ed25993e7b08e81e9f4d862b418814b2c1af9
Arne Neumann <arne-neumann@web.de>
2014-04-13 23:44:17 +0200

    imported tiger, anaphoricity and rst doc graphs into io namespace

---
+++ src/discoursegraphs/io/__init__.py
@@ -0,0 +1,5 @@
+# discoursegraphs.io: input/output functionality
+
+from discoursegraphs.io.tiger import TigerDocumentGraph
+from discoursegraphs.io.anaphoricity import AnaphoraDocumentGraph
+from discoursegraphs.io.rst import RSTGraph
