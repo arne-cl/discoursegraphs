97253621715170608dd4397eb77b6f6af9236596
Arne Neumann <discoursegraphs.programming@arne.cl>
2014-04-14 10:57:03 +0200

    merging: outsourced rst-tiger merging to function

--- src/discoursegraphs/merging.py
+++ src/discoursegraphs/merging.py
@@ -12,6 +12,35 @@ from discoursegraphs.readwrite.anaphoricity import AnaphoraDocumentGraph
 from discoursegraphs.readwrite.rst import RSTGraph, rst_tokenlist
 from discoursegraphs.readwrite.tiger import TigerDocumentGraph, tiger_tokenlist
 
+def add_rst_to_tiger(tiger_docgraph, rst_graph):
+    """
+    adds an RSTGraph to a TigerDocumentGraph, thereby adding edges from
+    each RST segment to the (Tiger) tokens they represent.
+
+    Parameters
+    ----------
+    tiger_docgraph : TigerDocumentGraph
+        multidigraph representing a syntax annotated (TigerXML) document
+    rst_graph : RSTGraph
+        multidigraph representing a RST annotated (RS3) document
+    """
+    tiger_tokens = tiger_tokenlist(tiger_docgraph)
+    rst_tokens = rst_tokenlist(rst_graph)
+
+    tiger_docgraph.add_nodes_from(rst_graph.nodes(data=True))
+    tiger_docgraph.add_edges_from(rst_graph.edges(data=True))
+
+    for i, (tiger_tok, tiger_sent_id, tiger_tok_id) in enumerate(tiger_tokens):
+        rst_token, rst_segment_node_id = rst_tokens[i]
+        if tiger_tok == rst_token:
+            tiger_docgraph.add_node(tiger_tok_id, layers={'rst', 'rst:token'},
+                                   attr_dict={'rst:token': rst_token})
+            tiger_docgraph.add_edge(int(rst_segment_node_id), tiger_tok_id,
+                                   layers={'rst', 'rst:token'})
+        else: # token mismatch
+            raise ValueError("Tokenization mismatch between:\n" \
+                "{0}\n{1}".format(tiger_filepath, rst_filepath))
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
@@ -25,26 +54,11 @@ if __name__ == '__main__':
 
         assert os.path.isfile(tiger_filepath)
         tiger_docgraph = TigerDocumentGraph(tiger_filepath)
-        tiger_tokens = tiger_tokenlist(tiger_docgraph)
-
         assert os.path.isfile(rst_filepath)
         rst_graph = RSTGraph(rst_filepath)
-        rst_tokens = rst_tokenlist(rst_graph)
 
-        tiger_docgraph.add_nodes_from(rst_graph.nodes(data=True))
-        tiger_docgraph.add_edges_from(rst_graph.edges(data=True))
-
-        for i, (tiger_tok, tiger_sent_id, tiger_tok_id) in enumerate(tiger_tokens):
-            rst_token, rst_segment_node_id = rst_tokens[i]
-            if tiger_tok == rst_token:
-                tiger_docgraph.add_node(tiger_tok_id, layers={'rst', 'rst:token'},
-                                       attr_dict={'rst:token': rst_token})
-                tiger_docgraph.add_edge(int(rst_segment_node_id), tiger_tok_id,
-                                       layers={'rst', 'rst:token'})
-            else: # token mismatch
-                raise ValueError("Tokenization mismatch between:\n" \
-                    "{0}\n{1}".format(tiger_filepath, rst_filepath))
+        add_rst_to_tiger(tiger_docgraph, rst_graph)
 
         for i, node in tiger_docgraph.nodes(data=True):
             print i, node
-        #~ write_gpickle(tiger_docgraph, pickle_filepath)
+        write_gpickle(tiger_docgraph, pickle_filepath)
