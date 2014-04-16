b8d79a3d787eced1349e5b8dd5267e311738f76d
Arne Neumann <discoursegraphs.programming@arne.cl>
2014-04-16 14:27:43 +0200

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    "lxml", "networkx", "pygraphviz"
 ]
 
 
