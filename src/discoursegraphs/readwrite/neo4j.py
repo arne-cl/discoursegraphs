2dec353f9a4fa3b976a574fbe70238ac213e8ac4
Arne Neumann <discoursegraphs.programming@arne.cl>
2014-05-05 17:30:11 +0200

    added geoff conversion to neo4j module

--- src/discoursegraphs/readwrite/neo4j.py
+++ src/discoursegraphs/readwrite/neo4j.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 # Author: Arne Neumann <discoursegraphs.programming@arne.cl>
 
-from neonx import write_to_neo
+from neonx import write_to_neo, get_geoff
 from discoursegraphs import DiscourseDocumentGraph
 
 def make_json_encodable(discoursegraph):
@@ -15,11 +15,31 @@ def make_json_encodable(discoursegraph):
     discoursegraph : DiscourseDocumentGraph
     """
     for node_id in discoursegraph:
-        discoursegraph.node[node_id]['layers'] = list(discoursegraph.node[node_id]['layers'])
+        discoursegraph.node[node_id]['layers'] = \
+            list(discoursegraph.node[node_id]['layers'])
     for (from_id, to_id) in adg.edges_iter():
-        edge_dict = adg.edge[from_id][to_id] # there might be multiple edges between 2 nodes
+        # there might be multiple edges between 2 nodes
+        edge_dict = adg.edge[from_id][to_id]
         for edge_id in edge_dict:
-            edge_dict[edge_id]['layers'] = list(edge_dict[edge_id]['layers'])
+            edge_dict[edge_id]['layers'] = \
+                list(edge_dict[edge_id]['layers'])
+
+
+def convert_to_geoff(discoursegraph):
+    """
+    Parameters
+    ----------
+    discoursegraph : DiscourseDocumentGraph
+        the discourse document graph to be converted into GEOFF format
+
+    Returns
+    -------
+    geoff : string
+        a geoff string representation of the discourse graph.
+    """
+    make_json_encodable(discoursegraphs)
+    return get_geoff(discoursegraph, 'LINKS_TO')
+
 
 def upload_to_neo4j(discoursegraph):
     """
@@ -35,4 +55,5 @@ def upload_to_neo4j(discoursegraph):
         list of results from the `write_to_neo` function of neonx.
     """
     make_json_encodable(discoursegraphs)
-    return write_to_neo("http://localhost:7474/db/data/", discoursegraph, 'LINKS_TO')
+    return write_to_neo("http://localhost:7474/db/data/",
+        discoursegraph, 'LINKS_TO')
