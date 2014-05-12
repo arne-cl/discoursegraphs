18d66bd9da8db926a6f02f635c5fd7cbbba62777
Arne Neumann <github@arne.cl>
2014-05-12 22:35:39 +0200

    added __version__, __author__ and __email__ to root __init__.py

--- src/discoursegraphs/__init__.py
+++ src/discoursegraphs/__init__.py
@@ -1,5 +1,9 @@
 # Example package with a console entry point
 
+__author__ = 'Arne Neumann'
+__email__ = 'discoursegraphs.programming@arne.cl'
+__version__ = '0.1.1'
+
 from dg import DiscourseDocumentGraph
 
 
