7d1f502fe481ee252abcbbb684c955303797a896
Arne Neumann <discoursegraphs.programming@arne.cl>
2014-05-15 15:33:00 +0200

    added node label attribute support to neo4j exporter

--- src/discoursegraphs/readwrite/neo4j.py
+++ src/discoursegraphs/readwrite/neo4j.py
@@ -9,6 +9,8 @@ string and/or exports it to a running ``Neo4j`` graph database.
 
 from neonx import write_to_neo, get_geoff
 from discoursegraphs import DiscourseDocumentGraph
+from discoursegraphs.util import ensure_utf8
+
 
 def make_json_encodable(discoursegraph):
     """
@@ -29,6 +31,21 @@ def make_json_encodable(discoursegraph):
             edge_dict[edge_id]['layers'] = \
                 list(edge_dict[edge_id]['layers'])
 
+def add_node_ids_as_labels(discoursegraph):
+    """
+    Adds the ID of each node of a discourse graph as a label (an attribute
+    named ``label`` with the value of the node ID) to itself. This will
+    ignore nodes whose ID isn't a string or which already have a label
+    attribute.
+
+    Parameters
+    ----------
+    discoursegraph : DiscourseDocumentGraph
+    """
+    for node_id, properties in discoursegraph.nodes_iter(data=True):
+        if not 'label' in properties and isinstance(node_id, (str, unicode)):
+            discoursegraph.node[node_id]['label'] = ensure_utf8(node_id)
+
 
 def convert_to_geoff(discoursegraph):
     """
@@ -43,6 +60,7 @@ def convert_to_geoff(discoursegraph):
         a geoff string representation of the discourse graph.
     """
     make_json_encodable(discoursegraph)
+    add_node_ids_as_labels(discoursegraph)
     return get_geoff(discoursegraph, 'LINKS_TO')
 
 
@@ -60,5 +78,7 @@ def upload_to_neo4j(discoursegraph):
         list of results from the `write_to_neo` function of neonx.
     """
     make_json_encodable(discoursegraph)
+    add_node_ids_as_labels(discoursegraph)
     return write_to_neo("http://localhost:7474/db/data/",
-        discoursegraph, edge_rel_name='LINKS_TO', edge_rel_attrib='edge_type')
+        discoursegraph, edge_rel_name='LINKS_TO',
+        edge_rel_attrib='edge_type', label_attrib='label')
