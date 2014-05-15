2cd8d3118e927b01f41c497681554650d2dc6b53
Arne Neumann <discoursegraphs.programming@arne.cl>
2014-05-15 17:29:52 +0200

    flake8: reformatted util.py

--- src/discoursegraphs/util.py
+++ src/discoursegraphs/util.py
@@ -49,6 +49,7 @@ def ensure_unicode(str_or_unicode):
                          "but its of type {1}".format(str_or_unicode,
                                                       type(str_or_unicode)))
 
+
 def ensure_utf8(str_or_unicode):
     """
     tests, if the input is ``str`` or ``unicode``. if it is ``unicode``,
@@ -60,6 +61,6 @@ def ensure_utf8(str_or_unicode):
     elif isinstance(str_or_unicode, unicode):
         return str_or_unicode.encode('utf-8')
     else:
-        raise ValueError("Input '{0}' should be a string or unicode, "
-                         "but it is of type {1}".format(str_or_unicode,
-                                                      type(str_or_unicode)))
+        raise ValueError(
+            "Input '{0}' should be a string or unicode, but it is of "
+            "type {1}".format(str_or_unicode, type(str_or_unicode)))
