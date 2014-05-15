750b1e47818c6c5ae140920f5dbc199b1d335cb4
Arne Neumann <discoursegraphs.programming@arne.cl>
2014-05-15 15:57:40 +0200

    cleaned root __init__.py

--- src/discoursegraphs/__init__.py
+++ src/discoursegraphs/__init__.py
@@ -1,11 +1,11 @@
-# Example package with a console entry point
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Arne Neumann <discoursegraphs.programming@arne.cl>
 
 __author__ = 'Arne Neumann'
 __email__ = 'discoursegraphs.programming@arne.cl'
 __version__ = '0.1.2'
 
+__all__ = ['DiscourseDocumentGraph']
+
 from dg import DiscourseDocumentGraph
-
-
-def main():
-    print "Hello World"
