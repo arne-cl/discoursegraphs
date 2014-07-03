de4eb6f94f10f6d87bf0fdd836f13cebc484d6bc
Arne Neumann <discoursegraphs.programming@arne.cl>
2014-07-03 17:31:34 +0200

    outsourced get_xsi_type to salt.util.py

---
+++ src/discoursegraphs/readwrite/salt/util.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Arne Neumann
+
+"""
+This module contains utility functions (and global variables) for dealing with
+SaltXMI files.
+"""
+
+NAMESPACES = {'xmi': 'http://www.omg.org/XMI',
+              'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+              'sDocumentStructure': 'sDocumentStructure',
+              'saltCore': 'saltCore'}
+
+def get_xsi_type(element):
+    """
+    returns the type of an element of the XML tree, i.e. nodes, edges,
+    layers etc.), raises an exception if the element has no 'xsi:type'
+    attribute.
+    """
+    nsdict = NAMESPACES
+    #.xpath() always returns a list, so we need to select the first element
+    try:
+        type_with_namespace = element.xpath('@xsi:type', namespaces=nsdict)[0]
+        salt_ns, element_type = type_with_namespace.split(':')
+        return element_type
+    except:
+        raise Exception, ("The '{0}' element has no 'xsi:type' but has these "
+            "attribs:\n{1}").format(element.tag, element.attrib)
