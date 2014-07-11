e3e19d436d611ef243aa92aed6944888ca536674
Arne Neumann <github@arne.cl>
2014-07-11 12:55:21 +0200

    fixes #61: SaltLayer now handles edges belonging to a layer

--- src/discoursegraphs/readwrite/salt/layers.py
+++ src/discoursegraphs/readwrite/salt/layers.py
@@ -23,8 +23,11 @@ class SaltLayer(SaltElement):
     nodes : list of int
         a list of node indices which point to the nodes belonging to this
         layer
+    edges : list of int
+        a list of edge indices which point to the edges belonging to this
+        layer
     """
-    def __init__(self, name, element_id, xsi_type, labels, nodes, xml=None):
+    def __init__(self, name, element_id, xsi_type, labels, nodes, edges, xml=None):
         """
         Parameters
         ----------
@@ -42,12 +45,15 @@ class SaltLayer(SaltElement):
         nodes : list of int
             a list of node indices which point to the nodes belonging to this
             layer
+        edges : list of int
+            a list of edge indices which point to the edges belonging to this
+            layer
         xml : lxml.etree._Element or None
             an etree element parsed from a SaltXML document
         """
         super(SaltLayer, self).__init__(name, element_id, xsi_type, labels, xml)
-        nodes_str = element.xpath('@nodes')[0]
         self.nodes = nodes
+        self.edges = edges
 
     @classmethod
     def from_etree(cls, etree_element):
@@ -58,6 +64,14 @@ class SaltLayer(SaltElement):
         ins = SaltElement.from_etree(etree_element)
         # TODO: this looks dangerous, ask Stackoverflow about it!
         ins.__class__ = SaltLayer.mro()[0]  # convert SaltElement into SaltLayer
-        nodes_str = etree_element.xpath('@nodes')[0]
-        ins.nodes = [int(node_id) for node_id in DIGITS.findall(nodes_str)]
+
+        # add nodes and edges that belong to this layer (if any)
+        for element in ('nodes', 'edges'):
+            elem_list = []
+            xpath_result = etree_element.xpath('@'+element)
+            if xpath_result:
+                val_str = xpath_result[0]
+                elem_list.extend(int(elem_id)
+                                 for elem_id in DIGITS.findall(val_str))
+            setattr(ins, element, elem_list)
         return ins
