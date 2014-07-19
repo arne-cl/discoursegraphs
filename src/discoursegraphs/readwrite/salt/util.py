b4f1880dc99a9ce13cc1e97536a8253d5b65fda7
Arne Neumann <github@arne.cl>
2014-07-19 10:14:50 +0200

    flake8: salt.util

--- src/discoursegraphs/readwrite/salt/util.py
+++ src/discoursegraphs/readwrite/salt/util.py
@@ -12,6 +12,7 @@ NAMESPACES = {'xmi': 'http://www.omg.org/XMI',
               'sDocumentStructure': 'sDocumentStructure',
               'saltCore': 'saltCore'}
 
+
 def get_xsi_type(element):
     """
     returns the type of an element of the XML tree (incl. its namespace),
@@ -22,6 +23,6 @@ def get_xsi_type(element):
     #.xpath() always returns a list, so we need to select the first element
     try:
         return element.xpath('@xsi:type', namespaces=nsdict)[0]
-    except:
-        raise Exception, ("The '{0}' element has no 'xsi:type' but has these "
-            "attribs:\n{1}").format(element.tag, element.attrib)
+    except IndexError:  # xpath result is empty
+        raise ValueError("The '{0}' element has no 'xsi:type' but has these "
+                         "attribs:\n{1}").format(element.tag, element.attrib)
