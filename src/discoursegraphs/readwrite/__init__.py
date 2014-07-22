6245ed4f2458a8edba018135190dca35e0da2db4
Arne Neumann <discoursegraphs.programming@arne.cl>
2014-07-22 14:48:07 +0200

    paula: added new formats to readwrite namespace

--- src/discoursegraphs/readwrite/__init__.py
+++ src/discoursegraphs/readwrite/__init__.py
@@ -8,12 +8,15 @@ processing.
 """
 
 __all__ = [
-    'TigerDocumentGraph', 'AnaphoraDocumentGraph', 'RSTGraph',
-    'MMAXDocumentGraph', 'ConanoDocumentGraph'
+    'AnaphoraDocumentGraph', 'ConanoDocumentGraph', 'MMAXDocumentGraph',
+    'PaulaDocument', 'write_paula', 'RSTGraph', 'SaltDocument', 'SaltXMIGraph',
+    'TigerDocumentGraph'
 ]
 
-from discoursegraphs.readwrite.tiger import TigerDocumentGraph
 from discoursegraphs.readwrite.anaphoricity import AnaphoraDocumentGraph
 from discoursegraphs.readwrite.conano import ConanoDocumentGraph
-from discoursegraphs.readwrite.rst import RSTGraph
 from discoursegraphs.readwrite.mmax2 import MMAXDocumentGraph
+from discoursegraphs.readwrite.paulaxml.paula import PaulaDocument, write_paula
+from discoursegraphs.readwrite.rst import RSTGraph
+from discoursegraphs.readwrite.salt.saltxmi import SaltDocument, SaltXMIGraph
+from discoursegraphs.readwrite.tiger import TigerDocumentGraph
