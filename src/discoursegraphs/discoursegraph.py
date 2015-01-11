830db26821ab836213a540ea1a68125e070544b4
Arne Neumann <github@arne.cl>
2015-01-12 00:58:31 +0100

    added documentation to create_token_mapping()

--- src/discoursegraphs/discoursegraph.py
+++ src/discoursegraphs/discoursegraph.py
@@ -602,6 +602,25 @@ def rename_tokens(docgraph_with_old_names, docgraph_with_new_names):
 
 
 def create_token_mapping(docgraph_with_old_names, docgraph_with_new_names):
+    """
+    given two document graphs which annotate the same text and which use the
+    same tokenization, creates a dictionary with a mapping from the token
+    IDs used in the first graph to the token IDs used in the second graph.
+
+    Parameters
+    ----------
+    docgraph_with_old_names : DiscourseDocumentGraph
+        a document graph with token IDs that will be replaced later on
+    docgraph_with_new_names : DiscourseDocumentGraph
+        a document graph with token IDs that will replace the token IDs
+        used in ``docgraph_with_old_names`` later on
+
+    Returns
+    -------
+    old2new : dict
+        maps from a token ID used in ``docgraph_with_old_names`` to the token
+        ID used in ``docgraph_with_new_names`` to reference the same token
+    """
     old_token_ids = list(docgraph_with_old_names.get_tokens())
     new_token_ids = docgraph_with_new_names.get_tokens()
 
