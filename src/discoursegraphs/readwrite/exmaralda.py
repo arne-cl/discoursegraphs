afddc2f9c346834ae3e67ed329fb8682a05bda45
Arne Neumann <discoursegraphs.programming@arne.cl>
2015-01-23 16:09:47 +0100

    implemented basic Exmaralda parser (no hierarchical interpretation of span tiers)

--- src/discoursegraphs/readwrite/exmaralda.py
+++ src/discoursegraphs/readwrite/exmaralda.py
@@ -4,11 +4,8 @@
 
 """
 The ``exmaralda`` module converts a ``DiscourseDocumentGraph`` (possibly
-containing multiple annotation layers) into an Exmaralda ``*.exb`` file.
-
-WARNING: This module contains lots of bad, academic code (i.e. I needed to get
-stuff done quickly for a presentation and didn't take the time to add
-parameters and refactor large methods).
+containing multiple annotation layers) into an Exmaralda ``*.exb`` file
+and vice versa.
 """
 
 import os
@@ -17,8 +14,10 @@ from collections import defaultdict
 from lxml import etree
 from lxml.builder import ElementMaker
 
-from discoursegraphs import (get_annotation_layers, get_pointing_chains,
-                             get_span, select_nodes_by_layer)
+from discoursegraphs import (DiscourseDocumentGraph, EdgeTypes,
+                             get_annotation_layers,
+                             get_pointing_chains, get_span,
+                             select_nodes_by_layer)
 from discoursegraphs.util import create_dir
 
 
@@ -273,6 +272,178 @@ class ExmaraldaFile(object):
         """
         return (self.toknode2id[span_node_ids[0]],
                 self.toknode2id[span_node_ids[-1]]+1)
+
+
+class ExmaraldaDocumentGraph(DiscourseDocumentGraph):
+    """graph representation of an Exmaralda-annotated document"""
+    def __init__(self, exmaralda_file, name=None, namespace='exmaralda',
+                 token_tier='tok', ignored_tier_categories=None):
+        """
+        generates a document graph from an Exmaralda *.exb file
+
+        Parameters
+        ----------
+        exmaralda_file : str
+            path to an *.exb file
+        name : str or None
+            name of the document graph. If None, will be set to the input
+            file's basename
+        namespace : str
+            namespace of the graph, default: exmaralda
+        token_tier: str
+            the category attribute of the <tier> that contains the tokens.
+            default: tok
+        ignored_tier_categories : None or list of str
+            a list of tier categories which will not be added to the document
+            graph
+        """
+        # super calls __init__() of base class DiscourseDocumentGraph
+        super(ExmaraldaDocumentGraph, self).__init__()
+        self.name = name if name else os.path.basename(exmaralda_file)
+        self.ns = namespace
+        self.root = self.ns+':root_node'
+        
+        tree = etree.parse(exmaralda_file)
+        self.tokens = []
+        
+        self.__add_tokenization(tree)
+        
+        if ignored_tier_categories:
+            for tier in tree.iter('tier'):
+                if tier.attrib['category'] not in ignored_tier_categories:
+                    self.__add_tier(tier, token_tier_name=token_tier)
+        else:
+            for tier in tree.iter('tier'):
+                self.__add_tier(tier, token_tier_name=token_tier)
+
+    def __add_tokenization(self, tree):
+        """adds a node for each token ID in the document"""
+        for token_id in self.get_token_ids(tree):
+            self.add_node(token_id, layers={self.ns})
+            self.tokens.append(token_id)
+    
+    def __add_tier(self, tier, token_tier_name):
+        """
+        adds a tier to the document graph (either as additional attributes
+        to the token nodes or as a span node with outgoing edges to the token
+        nodes it represents)
+        """
+        if tier.attrib['category'] == token_tier_name:
+            self.__add_tokens(tier)
+        else: 
+            if self.is_token_annotation_tier(tier):
+                self.__add_token_annotation_tier(tier)
+                
+            else:
+                self.__add_span_tier(tier)
+
+    def __add_tokens(self, token_tier):
+        """
+        adds all tokens to the document graph. Exmaralda considers them to
+        be annotations as well, that's why we could only extract the token
+        node IDs from the timeline (using ``__add_tokenization()``), but not
+        the tokens themselves.
+
+        Parameters
+        ----------
+        token_tier : etree._Element
+            an etree element representing the <tier> which contains the tokens
+        """
+        for event in token_tier.iter('event'):
+            assert len(self.gen_token_range(event.attrib['start'],
+                       event.attrib['end'])) == 1, \
+                "Events in the token tier must not span more than one token."
+            token_id = event.attrib['start']
+            self.node[token_id][self.ns+':token'] = event.text
+
+    def is_token_annotation_tier(self, tier):
+        """
+        returns True, iff all events in the given tier annotate exactly one
+        token.
+        """
+        for i, event in enumerate(tier.iter('event')):
+            if self.indexdelta(event.attrib['end'], event.attrib['start']) != 1:
+                return False
+        return True
+
+    def __add_token_annotation_tier(self, tier):
+        """
+        adds a tier to the document graph, in which each event annotates
+        exactly one token.
+        """
+        for i, event in enumerate(tier.iter('event')):
+            anno_key = '{}:{}'.format(self.ns, tier.attrib['category'])
+            anno_val = event.text if event.text else ''
+            self.node[event.attrib['start']][anno_key] = anno_val        
+
+    def __add_span_tier(self, tier):
+        """
+        adds a tier to the document graph in which each event annotates a span
+        of one or more tokens.
+        """
+        tier_id = tier.attrib['id']
+        # add the tier's root node with an inbound edge from the document root
+        self.add_node(
+            tier_id, layers={self.ns, self.ns+':tier'},
+            attr_dict={self.ns+':category': tier.attrib['category'],
+                       self.ns+':type': tier.attrib['type'],
+                       self.ns+':display-name': tier.attrib['display-name']})
+        self.add_edge(self.root, tier_id, edge_type=EdgeTypes.dominance_relation)
+        
+        # add a node for each span, containing an annotation.
+        # add an edge from the tier root to each span and an edge from each
+        # span to the tokens it represents
+        for i, event in enumerate(tier.iter('event')):
+            span_id = '{}_{}'.format(tier_id, i)
+            span_tokens = self.gen_token_range(event.attrib['start'], event.attrib['end'])
+            annotation = event.text if event.text else ''
+            self.add_node(
+                span_id, layers={self.ns, self.ns+':span'},
+                attr_dict={self.ns+':annotation': annotation,
+                           'label': annotation})
+            self.add_edge(tier_id, span_id, edge_type=EdgeTypes.dominance_relation)
+            
+            for token_id in span_tokens:
+                self.add_edge(span_id, token_id,
+                              edge_type=EdgeTypes.spanning_relation)
+
+    def get_token_ids(self, tree):
+        """
+        returns a list of all token IDs occuring the the given exmaralda file,
+        sorted by their time stamp in ascending order.
+        """
+        def tok2time(token_element):
+            '''
+            extracts the time (float) of a <tli> element
+            (i.e. the absolute position of a token in the document)
+            '''
+            return float(token_element.attrib['time'])
+
+        timeline = tree.find('//common-timeline')
+        return (tok.attrib['id'] 
+                for tok in sorted((tli for tli in timeline.iterchildren()),
+                                  key=tok2time))
+
+    def tokenid2index(self, token_id):
+        """converts a token ID (e.g. 'T0') to its index (i.e. 0)"""
+        return int(token_id[1:])
+
+    def indexdelta(self, stop_id, start_id):
+        return self.tokenid2index(stop_id) - self.tokenid2index(start_id)
+
+    def gen_token_range(self, start_id, stop_id):
+        """
+        returns a list of all token IDs in the given, left-closed,
+        right-open interval (i.e. includes start_id, but excludes stop_id)
+
+        >>> gen_token_range('T0', 'T1')
+        ['T0']
+        
+        >>> gen_token_range('T1', 'T5')
+        ['T1', 'T2', 'T3', 'T4']
+        """
+        index_range = range(self.tokenid2index(start_id), self.tokenid2index(stop_id))
+        return ["T{}".format(index) for index in index_range]
 
 
 def is_informative(layer):
