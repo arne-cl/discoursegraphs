9c8329f968757270fad2ca226e8f9c7b92c4e9c1
Arne Neumann <github@arne.cl>
2015-02-16 23:05:25 +0100

    exportxml: turned edges/relations into docgraph attributes

--- src/discoursegraphs/readwrite/exportxml.py
+++ src/discoursegraphs/readwrite/exportxml.py
@@ -16,8 +16,9 @@ much faster.
 
 import os
 from lxml import etree
-
 import igraph as ig
+
+import discoursegraphs as dg
 
 
 class ExportXMLDocumentGraph(ig.Graph):
@@ -28,6 +29,20 @@ class ExportXMLDocumentGraph(ig.Graph):
         ----------
         limit : int or None
             only parse the first n sentences (to save time, RAM etc.)
+
+        Attributes
+        ----------
+        _edges : list of (str, str)
+            a list of edges, represented as (source
+            node ID, target node ID) tuples. this will be used to cache
+            edges, as add_edge() is much slower than add_edges() in ``igraph``.
+        _edge_types : dict
+            maps from an edge (i.e. (source node ID, target node ID)) to
+            an EdgeTypes enum
+        _relations : dict
+            maps from an edge (i.e. (source node ID, target node ID)) to
+            its anaphoric relation type (e.g. 'coreference',
+            'expletive')
         """
         # super calls __init__() of base class ig.Graph
         super(ExportXMLDocumentGraph, self).__init__(directed=True)
@@ -40,29 +55,30 @@ class ExportXMLDocumentGraph(ig.Graph):
         # in igraph, adding a single edge is prohibitively slow,
         # as the whole index of the graph has to be rebuild!
         # to speed this up, store the edges in a list & call add_edges() once!
-        edges = []
-        relations = {}
+        self._edges = []
+        self._edge_types = {}
+        self._relations = {}
 
         treeiter = etree.iterparse(exportxml_filepath, tag='sentence')
         if limit:
             for i in xrange(limit):
                 try:
                     _action, sentence = treeiter.next()
-                    self.add_sentence(sentence, edges, relations)
+                    self.add_sentence(sentence)
                 except StopIteration as e:
                     break # we've already parsed all sentences in that file
         else: # parse all sentences
             for _action, sentence in treeiter:
-                self.add_sentence(sentence, edges, relations)
-        self.add_edges(edges)
+                self.add_sentence(sentence)
+        self.add_edges(self._edges)
 
         # igraph doesn't store nodes/edge names in a dict, so a lookup would be O(n)
         node_name2id = {node['name']: node.index for node in self.vs}
         edge_endpoints2id = {(edge.source, edge.target): edge.index
                              for edge in self.es}
 
-        for (source, target) in relations:
-            relation_type = relations[(source, target)]
+        for (source, target) in self._relations:
+            relation_type = self._relations[(source, target)]
             if target:
                 edge_endpoints = (node_name2id[source], node_name2id[target])
                 self.es[edge_endpoints2id[edge_endpoints]]['exportxml:relation_type'] = relation_type
@@ -70,25 +86,17 @@ class ExportXMLDocumentGraph(ig.Graph):
                 # there's no antecedent in case of an expletive anaphoric relation
                 self.vs[node_name2id[source]]['exportxml:anaphora_type'] = relation_type
 
-    def add_sentence(self, sentence, edges, relations):
+    def add_sentence(self, sentence):
         """
         Parameters
         ----------
         sentence : etree.Element
             etree representation of a sentence
             (syntax tree with coreference annotation)
-        edges : list of (str, str)
-            a (potentially empty) list of edges, represented as (source
-            node ID, target node ID) tuples. this will be used to cache
-            edges, as add_edge() is much slower than add_edges() in ``igraph``.
-        relations : dict
-            maps from an edge (i.e. (source node ID, target node ID)) to
-            its anaphoric relation type (e.g. 'coreference',
-            'expletive')
         """
         sent_root_id = sentence.attrib['origin']
         self.add_vertex(sent_root_id, label=sent_root_id)
-        edges.append((self.root, sent_root_id))
+        self._edges.append((self.root, sent_root_id))
 
         for element in sentence.iter('node', 'word', 'anaphora'):
             parent_element = element.getparent()
@@ -105,12 +113,12 @@ class ExportXMLDocumentGraph(ig.Graph):
                 node_attrs = {'{}:{}'.format(self.ns, key):val for (key, val) in element.attrib.items()}
                 self.add_vertex(element_id, label=element.attrib['cat'],
                                 **node_attrs)
-                edges.append((parent_id, element_id))
+                self._edges.append((parent_id, element_id))
             elif element.tag == 'word':
                 node_attrs = {'{}:{}'.format(self.ns, key):val for (key, val) in element.attrib.items()}
                 self.add_vertex(element_id, label=element.attrib['form'],
                                 **node_attrs)
-                edges.append((parent_id, element_id))
+                self._edges.append((parent_id, element_id))
 
             else: # element.tag == 'anaphora'
                 # <anaphora> doesn't have an ID, but it's tied to its parent element
@@ -120,11 +128,11 @@ class ExportXMLDocumentGraph(ig.Graph):
                     # there might be more than one antecedent
                     for antecedent_id in antecedent_str.split(','):
                         edge = (parent_id, antecedent_id)
-                        edges.append(edge)
-                        relations[edge] = relation_type
+                        self._edges.append(edge)
+                        self._relations[edge] = relation_type
                 else:
                     # there's no antecedent in case of an expletive anaphoric relation
-                    relations[(parent_id, None)] = relation_type
+                    self._relations[(parent_id, None)] = relation_type
 
 
 def parse_anaphora(anaphora, source_id):
