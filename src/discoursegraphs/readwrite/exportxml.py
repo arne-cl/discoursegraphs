1d680210ae45596eb08dd9fa678ebcd4200886c4
Arne Neumann <github@arne.cl>
2015-02-17 00:04:20 +0100

    exportxml: added node/edge types

--- src/discoursegraphs/readwrite/exportxml.py
+++ src/discoursegraphs/readwrite/exportxml.py
@@ -15,6 +15,7 @@ much faster.
 '''
 
 import os
+import sys
 from lxml import etree
 import igraph as ig
 
@@ -50,7 +51,7 @@ class ExportXMLDocumentGraph(ig.Graph):
         self.name = name if name else os.path.basename(exportxml_filepath)
         self.ns = namespace
         self.root = self.ns+':root_node'
-        self.add_vertex(self.root, layers={self.ns})
+        self.add_vertex(self.root, layers={self.ns}, node_type='root')
 
         # in igraph, adding a single edge is prohibitively slow,
         # as the whole index of the graph has to be rebuild!
@@ -77,7 +78,7 @@ class ExportXMLDocumentGraph(ig.Graph):
         edge_endpoints2id = {(edge.source, edge.target): edge.index
                              for edge in self.es}
 
-        for (source, target) in self._relations:
+        for (source, target) in self._relations: # add relation types to anaphora
             relation_type = self._relations[(source, target)]
             if target:
                 edge_endpoints = (node_name2id[source], node_name2id[target])
@@ -85,6 +86,16 @@ class ExportXMLDocumentGraph(ig.Graph):
             else:
                 # there's no antecedent in case of an expletive anaphoric relation
                 self.vs[node_name2id[source]]['exportxml:anaphora_type'] = relation_type
+
+        for (source, target) in self._edge_types: # add edge types to edges
+            edge_endpoints = (node_name2id[source], node_name2id[target])
+            edge_type = self._edge_types[(source, target)]
+            try:
+                self.es[edge_endpoints2id[edge_endpoints]]['edge_type'] = edge_type
+            except KeyError as e:
+                sys.stderr.write("Can't find edge '{}'. alledged type: {}\n".format(edge_endpoints, edge_type))
+                sys.stderr.write("source: {}, target: {}\n".format(self.vs[edge_endpoints[0]]['node_type'],
+                                                                   self.vs[edge_endpoints[1]]['node_type']))
 
     def add_sentence(self, sentence):
         """
@@ -95,8 +106,11 @@ class ExportXMLDocumentGraph(ig.Graph):
             (syntax tree with coreference annotation)
         """
         sent_root_id = sentence.attrib['origin']
-        self.add_vertex(sent_root_id, label=sent_root_id)
-        self._edges.append((self.root, sent_root_id))
+        self.add_vertex(sent_root_id, label=sent_root_id,
+                        node_type='sentence_root')
+        edge = (self.root, sent_root_id)
+        self._edges.append(edge)
+        self._edge_types[edge] = dg.EdgeTypes.dominance_relation
 
         for element in sentence.iter('node', 'word', 'anaphora'):
             parent_element = element.getparent()
@@ -109,16 +123,20 @@ class ExportXMLDocumentGraph(ig.Graph):
                 sys.stderr.write("Unexpected parent '{}' of element '{}'\n".format(parent_element, element))
             element_id = element.attrib.get('id') # <anaphora> doesn't have an ID
 
-            if element.tag == 'node':
-                node_attrs = {'{}:{}'.format(self.ns, key):val for (key, val) in element.attrib.items()}
-                self.add_vertex(element_id, label=element.attrib['cat'],
+            if element.tag in ('node', 'word'):
+                if element.tag == 'node':
+                    label = element.attrib['cat']
+                    node_type = 'cat'
+                else: # element.tag == 'word'
+                    label = element.attrib['form']
+                    node_type = 'token'
+                node_attrs = {'{}:{}'.format(self.ns, key):val
+                              for (key, val) in element.attrib.items()}
+                self.add_vertex(element_id, label=label, node_type=node_type,
                                 **node_attrs)
-                self._edges.append((parent_id, element_id))
-            elif element.tag == 'word':
-                node_attrs = {'{}:{}'.format(self.ns, key):val for (key, val) in element.attrib.items()}
-                self.add_vertex(element_id, label=element.attrib['form'],
-                                **node_attrs)
-                self._edges.append((parent_id, element_id))
+                edge = (parent_id, element_id)
+                self._edges.append(edge)
+                self._edge_types[edge] = dg.EdgeTypes.dominance_relation
 
             else: # element.tag == 'anaphora'
                 # <anaphora> doesn't have an ID, but it's tied to its parent element
@@ -129,6 +147,7 @@ class ExportXMLDocumentGraph(ig.Graph):
                     for antecedent_id in antecedent_str.split(','):
                         edge = (parent_id, antecedent_id)
                         self._edges.append(edge)
+                        self._edge_types[edge] = dg.EdgeTypes.pointing_relation
                         self._relations[edge] = relation_type
                 else:
                     # there's no antecedent in case of an expletive anaphoric relation
@@ -161,7 +180,6 @@ WARNING: academic ad-hoc code to export to CoNLL using igraph instead of
 networkx.
 
 TODO: add markable span annotation to each antecedent/anaphora during import
-TODO: add edge_type to each edge
 
 needed functions / functionality
 --------------------------------
