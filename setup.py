eccbaac4724548d8343e09f7d191ae3674fd4c4d
Arne Neumann <github@arne.cl>
2015-05-11 23:25:38 +0300

    added nltk to requirements

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
-    "enum", "lxml", "networkx", "pygraphviz",
+    "nltk", "enum", "lxml", "networkx", "pygraphviz",
     "brewer2mpl", "unidecode"
 ]
 
