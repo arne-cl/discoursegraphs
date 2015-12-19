20334589a74faca1618c58809194c665781d08da
Arne Neumann <github@arne.cl>
2015-12-19 20:18:37 +0100

    major breakthrough: added first test

---
+++ tests/test_corpora.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Arne Neumann <discoursegraphs.programming@arne.cl>
+
+import pytest
+
+import discoursegraphs as dg
+from discoursegraphs.corpora import pcc
+
+
+@pytest.mark.slowtest
+def test_pcc():
+    """
+    create document graphs for all PCC documents containing all annotation
+    layers.
+    """
+    assert len(pcc.document_ids) == 176
+
+    for doc_id in pcc.document_ids:
+        docgraph = pcc[doc_id]
+        assert isinstance(docgraph, dg.DiscourseDocumentGraph)
