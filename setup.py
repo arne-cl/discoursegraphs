eeff8dfbc59e550ea8501253549e69aafdd00266
Arne Neumann <github@arne.cl>
2016-03-25 20:49:36 +0100

    minor: removed references to "enum" library

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = [
     # List your project dependencies here.
     # For more details, see:
     # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
-    "nltk", "enum", "lxml", "networkx", "pygraphviz",
+    "nltk", "lxml", "networkx", "pygraphviz",
     "brewer2mpl", "unidecode", "neonx", "pydot2"
 ]
 
