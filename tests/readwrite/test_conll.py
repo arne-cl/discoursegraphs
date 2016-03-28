caadbc5d8adaa6cfff592dbf1c7d92782c588405
Arne Neumann <github@arne.cl>
2016-03-28 19:30:13 +0200

    added more tests / example files for CoNLL importer

--- tests/readwrite/test_conll.py
+++ tests/readwrite/test_conll.py
@@ -15,9 +15,11 @@ Basic tests for the CoNLL family of corpus formats.
 """
 
 CONLL_FILEPATH = os.path.join(dg.DATA_ROOT_DIR, 'conll2009-example.tsv')
+ELEFANT_2009_FILEPATH = os.path.join(dg.DATA_ROOT_DIR, 'conll2009-elefant-mate.tsv')
+ELEFANT_2010_FILEPATH = os.path.join(dg.DATA_ROOT_DIR, 'conll2010-elefant-mate.tsv')
 
 def test_read_conll():
-    """convert a tab-separated CoNLL-2009 file into a document graph"""
+    """convert tab-separated CoNLL (2009/2010) files into document graphs"""
     cdg = dg.read_conll(CONLL_FILEPATH)
 
     # add precedence relations between tokens
@@ -28,11 +30,16 @@ def test_read_conll():
         list(dg.select_edges_by(conll_prec, layer='conll:precedence')))
     assert len(conll_prec.tokens) == num_of_prec_rels == 78
 
+    # read a file with key|val morphological features
+    cdg_elefant = dg.read_conll(ELEFANT_2009_FILEPATH)
+    # read the "CoNLL-2010" format (produced by mate tools?)
+    cdg_elefant_2010 = dg.read_conll(
+        ELEFANT_2010_FILEPATH, conll_format='2010')
+
 
 def test_traverse_dependencies_up():
     """follow the dependency path backwards from the given node to the root"""
     cdg = dg.read_conll(CONLL_FILEPATH)
-    #~ import pudb; pudb.set_trace()
     traverse_dependencies_up(cdg, 's1_t8', node_attr=None)
     
     # example sentence: Chomsky is a major figure in analytical philosophy ...
