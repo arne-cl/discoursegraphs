d787efa892940ee7052f1d5d281672fc9c1b1e05
Arne Neumann <github@arne.cl>
2017-11-01 22:19:27 +0100

    rs3tree: added more tests

--- tests/readwrite/test_rs3tree.py
+++ tests/readwrite/test_rs3tree.py
@@ -13,64 +13,210 @@ from discoursegraphs.readwrite.rst.rs3 import RSTTree
 import discoursegraphs as dg
 
 
-# files to test
-"""
-['eins-zwei-drei-(joint-eins-and-zwei-and-drei).rs3',
-
- 'eins-zwei-drei-(circ-(circ-eins-to-zwei)-from-drei).rs3',
- 'foo-bar-only-segments.rs3',
- 'foo-bar-circ-foo-to-bar.rs3',
- 'eins-zwei-drei-(circ-(joint-eins-and-zwei)-to-drei).rs3',
- 'eins-zwei-drei-(elab-eins-from-(joint-zwei-and-drei).rs3',
- 'foo-bar-foo-conj-bar.rs3',
- 'eins-zwei-drei-(circ-eins-from-(joint-zwei-and-drei).rs3',
- 'eins-zwei-drei-only-segments.rs3',
- 'eins-zwei-drei-(circ-(circ-eins-to-zwei)-to-drei.rs3',
- 'eins-zwei-drei-(circ-(joint-eins-and-zwei)-from-drei).rs3',
- 'eins-zwei-drei-(circ-eins-to-(joint-zwei-and-drei).rs3',
- 'foo-bar-elab-foo-to-bar.rs3',
- 'foo-bar-circ-bar-to-foo.rs3',
-
- 'foo-bar-foo-joint-bar.rs3',
- 'foo-bar-elab-bar-to-foo.rs3',
- 'eins-zwei-drei-(circ-(circ-eins-from-zwei)-from-drei).rs3',
- 'eins-zwei-drei-(circ-(circ-eins-from-zwei)-to-drei).rs3']"""
-
-def get_filepath(rs3tree_example_filename):
+def example2tree(rs3tree_example_filename):
     """Return the absolute path of an example file."""
-    return os.path.join(
-        dg.DATA_ROOT_DIR, 'rs3tree', rs3tree_example_filename)
+    filepath = os.path.join(dg.DATA_ROOT_DIR, 'rs3tree',
+                            rs3tree_example_filename)
+    return RSTTree(filepath)
 
 
 def test_segments_only_trees():
     """Files without a single root must get a virtual one."""
 
     # minimal case: file without any segments
-    rs3_file = get_filepath("empty.rs3")
-    produced = RSTTree(rs3_file)
+    produced = example2tree("empty.rs3")
     expected = t("")
 
     assert expected == produced.tree
 
-    # one lonely segment
-    rs3_file = get_filepath('only-one-segment.rs3')
-    produced = RSTTree(rs3_file)
-    expected = t("foo")
-
-    assert expected == produced.tree
-
     # two segments w/out a root
-    rs3_file = get_filepath("foo-bar-only-segments.rs3")
-    produced = RSTTree(rs3_file)
+    produced = example2tree("foo-bar-only-segments.rs3")
     expected = t("virtual-root",
                  [("N", "foo"), ("N", "bar")])
 
     assert expected == produced.tree
 
     # three segments w/out a root
-    rs3_file = get_filepath('eins-zwei-drei-only-segments.rs3')
-    produced = RSTTree(rs3_file)
+    produced = example2tree('eins-zwei-drei-only-segments.rs3')
     expected = t("virtual-root",
                  [("N", "eins"), ("N", "zwei"), ("N", "drei")])
+
+    assert expected == produced.tree
+
+    # FIXME: one lonely segment; produces (N (foo))
+    produced = example2tree('only-one-segment.rs3')
+    expected = t("foo")
+
+    assert expected == produced.tree
+
+
+
+def test_single_nucsat_relation():
+    # FIXME: only contains root node
+    produced = example2tree("foo-bar-circ-foo-to-bar.rs3")
+    expected = t("circumstance", [
+        ("S", "foo"),
+        ("N", "bar")])
+    
+    assert expected == produced.tree
+
+    # FIXME: only contains root node
+    produced = example2tree("foo-bar-elab-foo-to-bar.rs3")
+    expected = t("elaboration", [
+        ("S", "foo"),
+        ("N", "bar")])
+
+    assert expected == produced.tree
+
+    # FIXME: only contains root node
+    produced = example2tree("foo-bar-circ-bar-to-foo.rs3")
+    expected = t("circumstance", [
+        ("N", "foo"),
+        ("S", "bar")])
+
+    assert expected == produced.tree
+
+    # FIXME: only contains root node
+    produced = example2tree("foo-bar-elab-bar-to-foo.rs3")
+    expected = t("elaboration", [
+        ("N", "foo"),
+        ("S", "bar")])
+
+    assert expected == produced.tree
+
+
+
+def test_single_nucnuc_relation():
+    # FIXME: superfluous N root
+    produced = example2tree("foo-bar-foo-joint-bar.rs3")
+    expected = t("joint", [
+        ("N", "foo"),
+        ("N", "bar")])
+
+    assert expected == produced.tree
+
+    # FIXME: superfluous N root
+    produced = example2tree("foo-bar-foo-conj-bar.rs3")
+    expected = t("conjunction", [
+        ("N", "foo"),
+        ("N", "bar")])
+
+    assert expected == produced.tree
+
+    # FIXME: superfluous N root
+    produced = example2tree('eins-zwei-drei-(joint-eins-and-zwei-and-drei).rs3')
+    expected = t("joint", [
+        ("N", "eins"),
+        ("N", "zwei"),
+        ("N", "drei")])
+    
+    assert expected == produced.tree
+
+
+def test_nested_nucsat_relation():
+    # correct
+    produced = example2tree('eins-zwei-drei-(circ-(circ-eins-from-zwei)-from-drei).rs3')
+    expected = t("circumstance", [
+        ("N", [
+            ("circumstance", [
+                ("N", "eins"),
+                ("S", "zwei")])]),
+        ("S", "drei")])
+
+    assert expected == produced.tree
+
+    # correct
+    produced = example2tree('eins-zwei-drei-(circ-(circ-eins-to-zwei)-from-drei).rs3')
+    expected = t("circumstance", [
+        ("N", [
+            ("circumstance", [
+                ("S", "eins"),
+                ("N", "zwei")])]),
+        ("S", "drei")])
+
+    assert expected == produced.tree
+
+    # FIXME: only contains root node
+    produced = example2tree('eins-zwei-drei-(circ-(circ-eins-from-zwei)-to-drei).rs3')
+    expected = t("circumstance", [
+        ("S", [
+            ("circumstance", [
+                ("N", "eins"),
+                ("S", "zwei")])]),
+        ("N", "drei")])
+
+    assert expected == produced.tree
+
+    # FIXME: only contains root node
+    produced = example2tree('eins-zwei-drei-(circ-(circ-eins-to-zwei)-to-drei.rs3')
+    expected = t("circumstance", [
+        ("S", [
+            ("circumstance", [
+                ("S", "eins"),
+                ("N", "zwei")])]),
+        ("N", "drei")])
+
+    assert expected == produced.tree
+
+
+def test_nested_nucsat_nucnuc_relation():
+    # CORRECT
+    produced = example2tree('eins-zwei-drei-(circ-eins-to-(joint-zwei-and-drei).rs3')
+    expected = t("circumstance", [
+        ("S", "eins"),
+        ("N", [
+            ("joint", [
+                ("N", "zwei"),
+                ("N", "drei")])])])
+
+    assert expected == produced.tree
+
+    # CORRECT
+    produced = example2tree('eins-zwei-drei-(circ-(joint-eins-and-zwei)-from-drei).rs3')
+    expected = t("circumstance", [
+        ("N", [
+            ("joint", [
+                ("N", "eins"),
+                ("N", "zwei")
+            ])
+        ]),
+        ("S", "drei")])
+
+    assert expected == produced.tree
+
+    # FIXME, only contains root node
+    produced = example2tree('eins-zwei-drei-(circ-eins-from-(joint-zwei-and-drei).rs3')
+    expected = t("circumstance", [
+        ("N", "eins"),
+        ("S", [
+            ("joint", [
+                ("N", "zwei"),
+                ("N", "drei")])])])
+
+    assert expected == produced.tree
+
+    # FIXME, only contains root node
+    produced = example2tree('eins-zwei-drei-(circ-(joint-eins-and-zwei)-to-drei).rs3')
+    expected = t("circumstance", [
+        ("S", [
+            ("joint", [
+                ("N", "eins"),
+                ("N", "zwei")
+            ])
+        ]),
+        ("N", "drei")])
+
+    assert expected == produced.tree
+
+    # FIXME, only root node
+    produced = example2tree('eins-zwei-drei-(elab-eins-from-(joint-zwei-and-drei).rs3')
+    expected = t("elaboration", [
+        ("N", [
+            ("joint", [
+                ("N", "eins"),
+                ("N", "zwei")
+            ])
+        ]),
+        ("S", "drei")])
 
     assert expected == produced.tree
